f7d27324bea18a17a3d5aaa3358f756dfeb4e1c8
Anand <anandabktda@gmail.com>
2014-10-27 21:52:26 -0700

    Ninth Program

---
+++ Switch.java
@@ -0,0 +1,15 @@
+class Switch{
+public static void main(Switch [] args){
+{
+for(int i=0;i<=2;i++);
+{
+switch(fvbfb)
+{
+case 0:System.out.println("zero");
+case 1:Sytem.out.println("one");
+case 2:System.out.println("two");
+default:Sytem.out.println("not correct");
+}
+}
+} }
+}
