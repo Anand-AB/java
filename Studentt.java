686fa86904357483fed83903748fcb583080bf60
Anand <anandabktda@gmail.com>
2014-10-27 21:51:30 -0700

    Eighth Program

---
+++ Studentt.java
@@ -0,0 +1,20 @@
+class Student
+{
+	public static void main(string []args){
+	int mrk1,mrk2,total;
+	String name;
+	Student(int m1,int m2,String name){
+	mrk1=m1;
+	mrk2=m2;
+	name=name;
+	}
+	void cal()
+	{
+	total=mrk1=mrk2;
+	}
+	void get()
+	{
+	System.out.println("mame="+name+"mark1="+mrk1+"mark2="+mrk2+"total="total);
+	}
+	}
+	}
